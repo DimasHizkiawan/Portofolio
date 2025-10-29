967c8e81a38bee0571e67dc4119106ccb2f38389
DimasHizkiawan <dimashizkiawan@gmail.com>
2025-10-29 18:17:16 +0700

    Fix homepage path for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Portfolio from './pages/Portfolio';
 import { Toaster } from './components/ui/toaster';
 import './App.css';
@@ -6,11 +6,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Portfolio />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Portfolio />} />
+      </Routes>
       <Toaster />
     </div>
   );
